a6c0a6be2541ee7bfe38e9acfd4d91a614e3bfb0
kavos113 <kohei.hadano@gmail.com>
2025-01-25 01:20:54 +0900

    ui2 wip

---
+++ shaders/ui/side_ui.hlsl
@@ -0,0 +1,53 @@
+cbuffer WorldMatrix : register(b0)
+{
+    matrix world;
+};
+
+cbuffer Material : register(b1)
+{
+    float4 ambient;
+    float4 diffuse;
+    float4 specular;
+};
+
+cbuffer CameraMatrix : register(b2)
+{
+    matrix view;
+    matrix projection;
+};
+
+struct PS_Input
+{
+    float4 pos : SV_POSITION;
+    float2 uv : TEXCOORD0;
+    float3 normal : NORMAL;
+};
+
+PS_Input vs(
+    float4 pos : POSITION, 
+    float2 uv : TEXCOORD, 
+    float4 normal : NORMAL
+)
+{
+    PS_Input vsOut;
+    vsOut.pos = mul(projection, mul(view, mul(world, pos)));
+    vsOut.uv = uv;
+    vsOut.normal = mul(world, normal);
+    return vsOut;
+}
+
+Texture2D alpha1_tex : register(t0);
+Texture2D alpha2_tex : register(t1);
+SamplerState sam : register(s0);
+
+float4 ps(PS_Input psIn) : SV_TARGET
+{
+    float3 light = normalize(float3(0.0, -1.0, -1.0));
+    float brightness = dot(psIn.normal, light);
+
+    float4 alpha1 = alpha1_tex.Sample(sam, psIn.uv);
+    float4 alpha2 = alpha2_tex.Sample(sam, psIn.uv);
+
+    float4 color = alpha1 * brightness + alpha2 * (1.0 - brightness);
+    return color;
+}
